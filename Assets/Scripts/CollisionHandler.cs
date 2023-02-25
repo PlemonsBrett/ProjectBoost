fb12ab64a10268cf82f7b51a69260c1144d99e30
Brett Plemons <Brett.Plemons@gmail.com>
2023-02-25 00:15:34 -0600

    Add Reload Level and Loading Next level on collisions.

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,7 +1,10 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+    [SerializeField] private float levelLoadDelay = 1f;
+
     private void OnCollisionEnter(Collision collision)
     {
         switch (collision.gameObject.tag)
@@ -10,14 +13,48 @@ public class CollisionHandler : MonoBehaviour
                 Debug.Log("OK");
                 break;
             case "Finish":
-                Debug.Log("Level Complete");
+                StartNextLevelSequence();
                 break;
             case "Fuel":
                 Debug.Log("Picked up fuel");
                 break;
             default:
-                Debug.Log("Dead");
+                StartCrashSequence();
                 break;
         }
+    }
+
+    private void ReloadLevel()
+    {
+        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+
+    private void LoadNextLevel()
+    {
+        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        var nextSceneIndex = currentSceneIndex + 1;
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+
+    private void StartCrashSequence()
+    {
+        // TODO: Add particle effects
+        // TODO: Add SFX upon crash
+        GetComponent<Movement>().enabled = false;
+        Invoke(nameof(ReloadLevel), levelLoadDelay);
+    }
+
+    private void StartNextLevelSequence()
+    {
+        // TODO: Add particle effects
+        // TODO: Add SFX upon crash
+        GetComponent<Movement>().enabled = false;
+        Invoke(nameof(LoadNextLevel), levelLoadDelay);
     }
 }
